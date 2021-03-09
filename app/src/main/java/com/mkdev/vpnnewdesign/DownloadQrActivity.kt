57f1ef08dc280a822099089d0b83a78bb165d037
Masoud <khoshkam@mahsan.net>
2021-03-09 17:47:01 +0330

    Added activity_download_qr.xml

---
+++ app/src/main/java/com/mkdev/vpnnewdesign/DownloadQrActivity.kt
@@ -0,0 +1,16 @@
+package com.mkdev.vpnnewdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mkdev.vpnnewdesign.WaveLoadingView.WaveLoadingView
+
+class DownloadQrActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download_qr)
+
+
+    }
+}
