c75164e34f05fb0094b160c318316dbf4d488a67
Masoud <khoshkam@mahsan.net>
2021-03-09 16:04:15 +0330

    Added activity_connect.xml

---
+++ app/src/main/java/com/mkdev/vpnnewdesign/ConnectActivity.kt
@@ -0,0 +1,11 @@
+package com.mkdev.vpnnewdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect)
+    }
+}
