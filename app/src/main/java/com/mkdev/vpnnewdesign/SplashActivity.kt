8414b0bc3390eba3dfcc45509138f7910c986bae
Masoud <khoshkam@mahsan.net>
2021-03-09 14:31:44 +0330

    Added activity_splash

--- app/src/main/java/com/mkdev/vpnnewdesign/SplashActivity.kt
+++ app/src/main/java/com/mkdev/vpnnewdesign/SplashActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
 
     }
 }
---
+++ app/src/main/java/com/mkdev/vpnnewdesign/VpnApp.kt
@@ -0,0 +1,13 @@
+package com.mkdev.vpnnewdesign
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class VpnApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
