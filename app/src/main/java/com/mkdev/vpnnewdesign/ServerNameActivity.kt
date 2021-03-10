77f55e30320b5ee3c7f843ffaef2e49c33279b6d
Masoud <khoshkam@mahsan.net>
2021-03-10 14:54:44 +0330

    Added activity_server_name.xml

---
+++ app/src/main/java/com/mkdev/vpnnewdesign/ServerNameActivity.kt
@@ -0,0 +1,11 @@
+package com.mkdev.vpnnewdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServerNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server_name)
+    }
+}
