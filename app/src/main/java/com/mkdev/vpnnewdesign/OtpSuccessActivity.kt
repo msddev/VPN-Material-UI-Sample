b172ac1daea6e47d260ce4e732dd33fda6f97ffa
Masoud <khoshkam@mahsan.net>
2021-03-13 15:55:06 +0330

    Added activity_otp_success.xml

---
+++ app/src/main/java/com/mkdev/vpnnewdesign/OtpSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.mkdev.vpnnewdesign
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OtpSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_success)
+    }
+}
