563f8c0c429b598e35f2714c2ec371fa4cb12ee5
Masoud <khoshkam@mahsan.net>
2021-03-13 14:34:43 +0330

    Added count down timer

--- app/src/main/java/com/mkdev/vpnnewdesign/OtpCodeActivity.kt
+++ app/src/main/java/com/mkdev/vpnnewdesign/OtpCodeActivity.kt
@@ -1,10 +1,12 @@
 package com.mkdev.vpnnewdesign
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_otp_code.*
+
 
 class OtpCodeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +22,18 @@ class OtpCodeActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please fill all blank box!", Toast.LENGTH_LONG).show()
             }
         }
+
+        object : CountDownTimer(120000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val minute = String.format("%02d", millisUntilFinished / (60 * 1000) % 60)
+                val second = String.format("%02d", millisUntilFinished / 1000 % 60)
+                otpCodeTimer.text = "$minute:$second"
+                //here you can have your logic to set text to edittext
+            }
+
+            override fun onFinish() {
+                otpCodeTimer.text = "00:00"
+            }
+        }.start()
     }
 }
