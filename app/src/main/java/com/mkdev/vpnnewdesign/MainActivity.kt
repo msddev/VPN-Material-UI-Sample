d737edd07cbbaefef1577fbff0c876cfe277179a
Masoud <khoshkam@mahsan.net>
2021-03-14 17:02:57 +0330

    Added activity_main.xml

--- app/src/main/java/com/mkdev/vpnnewdesign/MainActivity.kt
+++ app/src/main/java/com/mkdev/vpnnewdesign/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.mkdev.vpnnewdesign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.app_bar_detail.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
 
+    private fun setupToolbar() {
+        detailToolbar.setNavigationOnClickListener {
+            //handleBackPress()
+        }
+        detailToolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24)
     }
 }
