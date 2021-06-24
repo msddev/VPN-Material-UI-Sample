a305850a57e7f9c44067ec1c8eb7ca5989922f7b
Masoud <khoshkam@mahsan.net>
2021-06-24 16:03:25 +0430

    Added log activity

---
+++ app/src/main/java/com/mkdev/vpnnewdesign/LogsActivity.kt
@@ -0,0 +1,13 @@
+package com.mkdev.vpnnewdesign
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logs)
+
+
+    }
+}
